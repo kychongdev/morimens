91bdc494c061878e42e1650dc326c0a7a19d63e6
kychongdev <kychongdev@gmail.com>
2023-12-21 03:29:46 +0800

    Add dynamic data to page

--- src/data/deepsea.ts
+++ src/data/deepsea.ts
@@ -1,8 +1,10 @@
+import tulu from '../assets/O01___A_1.png'
 export const data = [
   {
     id: 0,
     name: "圖魯",
     charType: "傷害型",
+    image: tulu,
     breakthrough:
       " 暗夜呢喃\n-「深夜號令」使觸腕攻擊所有敵人.\n甦醒之觸\n-「打擊」和「防禦」使觸腕傷害臨時提高圖魯攻擊的3%.（效果隨「打擊」,「防禦」的等級提升)\n群星歸位\n「」提高15%臨時暴擊率.",
   },
--- src/pages/Character.tsx
+++ src/pages/Character.tsx
@@ -1,31 +1,67 @@
-import tulu from '../assets/O01___A_1.png'
-import { Card, CardHeader, CardTitle, CardDescription, CardContent } from '@/components/ui/card'
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from "@/components/ui/card";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
-export const CharacterPage = ()=>{
+import { data } from "@/data/deepsea";
+import { useState } from "react";
+
+export const CharacterPage = () => {
+  const [char, setChar] = useState("");
+  const charInfo = data[parseInt(char)];
   return (
     <>
-      <Card>
-        <CardHeader>
-          <CardTitle>圖魯</CardTitle>
-          <CardDescription>傷害型</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <img src={tulu} alt='tulu' className="w-120" />
-        </CardContent>
-      </Card>
-      <Tabs defaultValue="skill" className="w-400 text-left">
-        <TabsList>
-          <TabsTrigger value="skill">技能</TabsTrigger>
-          <TabsTrigger value="breakthrough">啟靈</TabsTrigger>
-        </TabsList>
-        <TabsContent value="skill" className='text-left'>不巧威儀</TabsContent>
-        <TabsContent value="breakthrough" className='text-left'><p>
-          暗夜呢喃 <br />-「深夜號令」使觸腕攻擊所有敵人.<br />
-          甦醒之觸 <br />-「打擊」和「防禦」使觸腕傷害臨時提高圖魯攻擊的3%.（效果隨「打擊」,「防禦」的等級提升)<br />
-          群星歸位 <br /> 「」提高15%臨時暴擊率.</p>
-        </TabsContent>
-      </Tabs>
-    </>)
-}
+      <Select value={char} onValueChange={(value) => setChar(value)}>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="請選擇深海角色" />
+        </SelectTrigger>
+        <SelectContent>
+          {data.map((item, index) => {
+            return (
+              <SelectItem key={index} value={item.id.toString()}>
+                {item.name}
+              </SelectItem>
+            );
+          })}
+        </SelectContent>
+      </Select>
 
+      {char && char !== "" ? (
+        <>
+          <Card>
+            <CardHeader>
+              <CardTitle>{charInfo.name}</CardTitle>
+              <CardDescription>{charInfo.charType}</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <img src={charInfo.image} alt={charInfo.name} className="w-120" />
+            </CardContent>
+          </Card>
+          <Tabs defaultValue="skill" className="w-400 text-left">
+            <TabsList>
+              <TabsTrigger value="skill">技能</TabsTrigger>
+              <TabsTrigger value="breakthrough">啟靈</TabsTrigger>
+            </TabsList>
+            <TabsContent value="skill" className="text-left whitespace-pre-line">
+              不巧威儀
+            </TabsContent>
+            <TabsContent value="breakthrough" className="text-left whitespace-pre-line">
+              {charInfo.breakthrough}
+            </TabsContent>
+          </Tabs>
+        </>
+      ) : null}
+    </>
+  );
+};
