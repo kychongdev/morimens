fe38747069c27714df4d43e997e1fd41fef3e867
kychongdev <kychongdev@gmail.com>
2023-12-28 00:43:32 +0800

    Add boss page

--- src/pages/Boss.tsx
+++ src/pages/Boss.tsx
@@ -1,33 +1,34 @@
-import { Image } from "@mantine/core";
-import boss1 from "../assets/1.png";
+import { Button, Image } from "@mantine/core";
 import { BossButton } from "@/components/BossButton";
 import { useState } from "react";
+import { bossData } from "@/data/boss";
 import { BossInfo } from "./BossInfo";
+import { IconArrowBackUp } from "@tabler/icons-react";
 
 export const Boss = () => {
   const [boss, setBoss] = useState("");
+
   return (
     <>
-      {boss == "" ? (
-        <div className="flex flex-wrap gap-2 md:gap-5">
-          <BossButton onClick={() => setBoss("test")}>
-            <Image src={boss1} alt={"boss"} />
-          </BossButton>
-          <BossButton onClick={() => console.log("success")}>
-            <Image src={boss1} alt={"boss"} />
-          </BossButton>
-          <BossButton onClick={() => console.log("success")}>
-            <Image src={boss1} alt={"boss"} />
-          </BossButton>
-          <BossButton onClick={() => console.log("success")}>
-            <Image src={boss1} alt={"boss"} />
-          </BossButton>
-          <BossButton onClick={() => console.log("success")}>
-            <Image src={boss1} alt={"boss"} />
-          </BossButton>
-        </div>
+      {boss == ""
+        ? bossData.map((item) => {
+            return (
+              <div className="flex flex-wrap gap-2 md:gap-5">
+                <BossButton onClick={() => setBoss(item.id.toString())}>
+                  <Image src={item.img} alt={item.name} />
+                </BossButton>
+              </div>
+            );
+          })
+        : null}
+      {boss !== "" ? (
+        <>
+          <Button color="yellow" onClick={() => setBoss("")}>
+            <IconArrowBackUp />
+          </Button>
+          <BossInfo bossId={boss} />
+        </>
       ) : null}
-      {boss == "test" ? <BossInfo /> : null}
     </>
   );
 };
--- src/pages/BossInfo.tsx
+++ src/pages/BossInfo.tsx
@@ -1,15 +1,95 @@
-import { Text, Paper } from "@mantine/core";
+import {
+  Text,
+  Paper,
+  Image,
+  Accordion,
+  rem,
+  List,
+  ThemeIcon,
+  Title,
+  Divider,
+} from "@mantine/core";
+import {
+  // IconAlertTriangle,
+  IconQuestionMark,
+  // IconShield,
+  IconSword,
+  // IconSwords,
+} from "@tabler/icons-react";
+import test from "../assets/boss/1.png";
+import { bossData } from "@/data/boss";
 
-export const BossInfo = () => {
+interface BossInfoProps {
+  bossId: string;
+}
+
+export const BossInfo = ({ bossId }: BossInfoProps) => {
+  let data = bossData[parseInt(bossId)];
   return (
     <>
-      <Paper shadow="xs" radius="xl" withBorder p="xl">
-        <Text>Paper is the most basic ui component</Text>
-        <Text>
-          Use it to create cards, dropdowns, modals and other components that
-          require background with shadow
-        </Text>
+      <Title className="text-center" c={"yellow.6"} order={2}>
+        {data.name}
+      </Title>
+      <Image src={test} />
+      <Paper className="my-5" shadow="xs" radius="xl" withBorder p="xl">
+        <Text>{data.story}</Text>
       </Paper>
+      <div className="px-3">
+        <List spacing="xs">
+          {data.skill.map((item) => {
+            return (
+              <>
+                <List.Item
+                  icon={
+                    <ThemeIcon variant="default" size={24} radius="xl">
+                      <IconSword
+                        className="items-start"
+                        style={{ width: rem(16), height: rem(16) }}
+                      />
+                    </ThemeIcon>
+                  }
+                >
+                  <Text size="sm" c="yellow.6">
+                    <b>{item.name}</b>
+                  </Text>
+                </List.Item>
+                <Text size="sm">{item.info}</Text>
+                {item.passive !== undefined
+                  ? item.passive.map((item2) => {
+                      return (
+                        <Text size="sm" c="blue.6">
+                          <b>{item2.info}</b>
+                        </Text>
+                      );
+                    })
+                  : null}
+              </>
+            );
+          })}
+        </List>
+      </div>
+      <Accordion className="mt-5" variant="contained">
+        {data.question.map((item) => {
+          return (
+            <Accordion.Item value={item.value.toString()}>
+              <Accordion.Control
+                icon={
+                  <IconQuestionMark
+                    style={{
+                      color: "var(--mantine-color-red-6)",
+                      width: rem(20),
+                      height: rem(20),
+                    }}
+                  />
+                }
+              >
+                {item.title}
+              </Accordion.Control>
+              <Accordion.Panel>{item.info}</Accordion.Panel>
+            </Accordion.Item>
+          );
+        })}
+      </Accordion>
     </>
   );
 };
